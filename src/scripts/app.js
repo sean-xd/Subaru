af9500947d346f973056f6ea76fcdf140d27134b
Sean Murray <thecanbeat@gmail.com>
2016-05-05 23:31:15 -0600

    everything except editting groups
    t minus 13 hours ish

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,14 +2,14 @@
 var ls = localStorage,
   player,
   playing = false,
+  activeGroup = "Music",
+  theatre = false,
   bg = el(".bg")[0],
+  content = el(".content")[0]
   main = el("main")[0],
   spacer = el(".spacer")[0],
   header = el(".drawer")[0],
-  settingsIcon = el(".settingsIcon")[0],
-  settingsPanel = el(".settingsPanel")[0],
   videoIcon = el(".videoIcon")[0],
-  videoPanel = el(".videoPanel")[0],
   footer = el("footer")[0],
   sections = {},
   channels = ls.channels ? JSON.parse(ls.channels) : {},
@@ -18,7 +18,7 @@ var ls = localStorage,
 var list = {};
 
 // Main
-if(!ls.groups) group("Music", ["EpicNetworkMusic", "UDUBSTEPHD"]);
+if(!ls.groups)group("Music", ["EpicNetworkMusic", "UDUBSTEPHD"]);
 Object.keys(groups).forEach(load);
 
 // Functions
@@ -46,9 +46,9 @@ function load(name){ // Gets and passes group video data to draw.
 
 function format(data){
   var result = data.items.map(item => {
-    var e = item.snippet;
+    var e = item.snippet, d = new Date(e.publishedAt);
     return {
-      date: dat(new Date(e.publishedAt)).t,
+      date: d.getTime(),
       title: e.title,
       src: e.thumbnails.medium.url,
       id: e.resourceId.videoId,
@@ -61,7 +61,7 @@ function format(data){
 
 function draw(name){
   if(!groups[name]) return;
-  if(!sections[name]) sections[name] = t("section")(t("h3", {classes: ["group"]})(name));
+  if(!sections[name]) sections[name] = t("section", {id: name})(t("h3", {classes: ["group"]})(name));
   list[name] = groups[name]
     .reduce((arr, cn) => arr.concat(channels[cn]), [])
     .sort(sorter(e => Date.now() - e.date));
@@ -84,5 +84,60 @@ function channel(name, cb){
 
 function videos(id, cb){
   if(is(id, "Object")) id = id.items[0].id;
-  hp(`https://www.googleapis.com/youtube/v3/playlistItems?key=${apikey}&playlistId=UU${id.slice(2)}&part=snippet&maxResults=50`, cb);
+  hp(`https://www.googleapis.com/youtube/v3/playlistItems?key=${apikey}&playlistId=UU${id.slice(2)}&part=snippet&maxResults=24`, cb);
+}
+
+// Events
+videoIcon.addEventListener("click", toggleDrawer);
+
+function toggleDrawer(name){
+  if(is(name, "String")) activeGroup = name;
+  theatre = !theatre;
+  clt(header, "open");
+  clt(bg, "short");
+  clt(spacer, "space");
+  clt(el("#" + activeGroup), "expand");
+}
+
+main.addEventListener("click", e => {
+  if(clc(e.target, "group")) toggleDrawer(e.target.textContent);
+  if(clc(e.target, "video-img") || clc(e.target, "video-title")){
+    var id = pa(e.target).id;
+    if(!theatre) toggleDrawer(el("h3", pa(pa(e.target)))[0].textContent);
+    if(playing !== id){
+      playing = id;
+      return player.loadPlaylist(list[activeGroup], list[activeGroup].indexOf(id));
+    }
+    (player.getPlayerState() === 5) ? player.playVideo() : player.stopVideo();
+  }
+});
+
+el(".next")[0].addEventListener("click", () => {
+  playing = list[activeGroup][list[activeGroup].indexOf(playing) + 1];
+  player.nextVideo();
+});
+el(".prev")[0].addEventListener("click", () => {
+  if(list[activeGroup].indexOf(playing) < 1) return;
+  playing = list[activeGroup][list[activeGroup].indexOf(playing) - 1];
+  player.previousVideo();
+});
+el(".top")[0].addEventListener("click", () => {content.scrollTop = 0;});
+
+function onYouTubeIframeAPIReady(){
+  player = new YT.Player("player", {
+    playerVars: {controls: 1, showinfo: 0, iv_load_policy: 3},
+    events: {
+      onStateChange: onPlayerStateChange,
+      onReady: () => {
+        player.cuePlaylist(list[activeGroup]);
+      }
+    },
+    videoId: el(".video")[0] ? el(".video")[0].id : list[activeGroup] ? list[activeGroup][0] : "MhYqKg3oSQ8"
+  });
+}
+
+function onPlayerStateChange(e){
+  if(e.data === 0) playing = list[activeGroup][list[activeGroup].indexOf(playing) + 1];
+  if(el(".active")[0] && (e.data === -1 || e.data === 1)) clr(el(".active")[0], "active");
+  if(el("#" + playing) && e.data === 1) cla(el("#" + playing), "active");
 }
--- src/scripts/util.js
+++ src/scripts/util.js
@@ -1,4 +1,3 @@
-// DOM
 function el(id, parent){
   if(!parent) parent = document;
   if(id[0] === "#") return parent.getElementById(id.substr(1));
@@ -44,7 +43,6 @@ function cla(e, cn){return e.classList.add(cn);}
 function clr(e, cn){return e.classList.remove(cn);}
 function clt(e, cn){return e.classList.toggle(cn);}
 
-// Logic
 function hp(url, cb){
   var req = new XMLHttpRequest();
   req.addEventListener("load", function(){
@@ -53,13 +51,6 @@ function hp(url, cb){
   });
   req.open("GET", url);
   req.send();
-}
-
-function c1(cb, a){
-  return function(b){cb(a, b);};
-}
-function c2(cb, b){
-  return function(a){cb(a, b);};
 }
 
 function Magic(num, fn, check){
@@ -80,46 +71,5 @@ function sorter(check, backup){
   return function(a, b){
     if(check(a) === check(b) && backup) return backup(a) - backup(b);
     return check(a) - check(b);
-  }
-}
-
-// Events
-function Kyp(){
-  var handler = {},
-    codes = {
-      backspace: 8, tab: 9, enter: 13, shift: 16, ctrl: 17,
-      alt: 18, esc: 27, left: 37, up: 38, right: 39,
-      down: 40, 0: 48, 1: 49, 2: 50, 3: 51,
-      4: 52, 5: 53, 6: 54, 7: 55, 8: 56,
-      9: 57, a: 65, b: 66, c: 67, d: 68,
-      e: 69, f: 70, g: 71, h: 72, i: 73,
-      j: 74, k: 75, l: 76, m: 77, n: 78,
-      o: 79, p: 80, q: 81, r: 82, s: 83,
-      t: 84, u: 85, v: 86, w: 87, x: 88,
-      y: 89, z: 90, f1: 112, f2: 113, f3: 114,
-      f4: 115, f5: 116, f6: 117, f7: 118, f8: 119,
-      f9: 120, f10: 121, f11: 122, f12: 123
-    },
-    kyp = function(e, cb){
-      if(is(e, "String")) handler[codes[e]] = cb;
-      if(is(e, "Number")) handler[e] = cb;
-      if(!cb && handler[e.keyCode]) handler[e.keyCode](e);
-    };
-  window.addEventListener("keydown", kyp);
-  return kyp;
-}
-
-// Date
-function dat(date){
-  var h = date.getHours(), mi = date.getMinutes();
-  return {
-    y: date.getFullYear(), mo: date.getMonth() + 1,
-    d: date.getDate(), w: date.getDay(),
-    h: h, h12: (h > 12) ? h - 12 : h, ampm: (h > 11) ? "pm" : "am",
-    mi: (mi < 10) ? `0${mi}` : mi.toString(),
-    hmi: (h * 100) + mi,
-    sea: ["winter", "spring", "summer", "fall"][Math.floor(date.getMonth() / 3)],
-    sn: Math.floor(date.getMonth() / 3),
-    t: date.getTime()
   }
 }
